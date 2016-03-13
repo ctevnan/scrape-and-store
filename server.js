9eaf721959932e3f13bb189732cf76a027dda554
ctevnan <carolyntevnan@yahoo.com>
2016-03-12 19:12:37 -0500

    added cheerio, functioning properly with cnn.com, next up, append data to tables, use jQuery or bootstrap

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // npm packages
-
 var express = require('express');
 var expressHandlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
@@ -8,6 +7,18 @@ var mongoose = require('mongoose');  //orm thing
 var request = require('request');
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+request('http://www.cnn.com', function(error, response, body) {
+  //hand html response to cheerio
+  //assign that to a local $ var to provide familiar jQuery syntax
+  var $ = cheerio.load(body);
+  console.log($);
+
+  //same code used in browser
+  $('h2').each(function() {
+    console.log($(this).text());
+  });
+});  
 
 //routes
 var routes = require('./routes/index');
@@ -22,8 +33,6 @@ app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //db connection 
-
 app.listen(PORT, function() {
   console.log("Listening on:" + PORT);
-});
- 
+}); 
