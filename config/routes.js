a2e9567fb272f122d8243e1ea1d41f7c0e1bbe26
ctevnan <carolyntevnan@yahoo.com>
2016-04-01 17:43:37 -0400

    working on routes.js, added controllers and scripts dirs, moving code to its appropriate place

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var scrape = require('../scripts/scrape.js');
+var headlinesController = require('../controllers/headlinesController.js');
+var notesController = require('../controllers/notes.js');
+
 router.get('/', function (req, res) {
   res.render('home');
 });
+
