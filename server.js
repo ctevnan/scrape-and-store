85b49aa0005567e659dfb43fe78107e9d7a4d17b
ctevnan <carolyntevnan@yahoo.com>
2016-04-01 16:57:03 -0400

    added dirs, placed code that wasnt supposed to be in server.js into temp storage

--- server.js
+++ server.js
@@ -32,108 +32,16 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+//morgan
+app.use(logger('dev'));
+
 //routes
 var routes = require('./config/routes.js');
 
 app.use('/', routes);
-app.use('/test', routes);
-
-request('http://www.cnn.com', function(error, response, body) {
-  //hand html response to cheerio
-  //assign that to a local $ var to provide familiar jQuery syntax
-  var $ = cheerio.load('<h2 class="title">Title</h2>');
-
-  $('h2.title').text('Welcome to The Daily News Brief featuring CNN.com');
-  $('h2').addClass('Welcome');
-
-  $.html();
-  //console.log('h2' + $);
-
-  //same code used in browser
-  $('h2').each(function() {
-    console.log($(this).text());
-  });
-});  
-
-app.get('/', function (req, res) {
-  res.send(index.html);
-});
-
-app.use(logger('dev'));
-
-//req schemas
-var Note = require('./models/notemodel.js');
-var User = require('./models/usermodel.js');  
-
-//lets see if I can get a fake user working  //Batman working!
-var practiceUser = new User({
-  name: "Bruce Wayne"
-});
-
-//practiceUser.save(function (err, doc) {
-//  if (err) {
-//    console.log(err);
-//  } else {
-//    console.log(doc);  
-//  }
-//});
-
-//making a new note
-app.post('/submit', function (req, res) {
-  var newNote = new Note (req.body);
-
-//saving note
-  newNote.save(function (err, doc) {
-    if (err) {
-      res.send(err);
-    } else {
-
-//find user, push note id into the user's note array
-      User.findOneAndUpdate({}, {$push: {'notes': doc._id}}, {new: true}, function (err, doc) {
-        if (err) {
-          res.send(err);
-        } else {
-          res.send(doc);
-        }
-      });      
-    }
-  });  
-});
-
-//route to notes
-app.get('/notes', function (req, res) {
-  Note.find({}, function (err, doc) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(doc);
-    }
-  });
-});
-
-//route to see user w/o populating
-app.get('/user', function (req, res) {
-  User.find({}, function (err, doc) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(doc);
-    }
-  });
-});
-
-//route to see user w/populating
-app.get('/populateduser', function (req, res) {
-  populateduser.find({}, function (err, doc) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(doc);
-    }
-  })
-});
+app.use('/test', routes); 
 
 //server connection 
-app.listen(PORT, function() {
-  console.log("Listening on:" + PORT);
+app.listen(port, function() {
+  console.log("Listening on port:" + port);
 }); 
