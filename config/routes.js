139a8712d41e732c6c4328b1e0d67136598b9958
ctevnan <carolyntevnan@yahoo.com>
2016-04-26 13:21:21 -0400

    added .gitignore file, added node modules, adding routes

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,22 @@ var scrape = require('../scripts/scrape.js');
 var headlinesController = require('../controllers/headlinesController.js');
 var notesController = require('../controllers/notes.js');
 
+//use callback to return json data 
 router.get('/', function (req, res) {
   res.render('home');
+});
+
+//route to test scrape
+router.get('/test', function (req, res) {
+  scrape("http://www.cnn.com", function (data) {
+    res.json(data);
+  });
+});
+
+//get web scrape
+router.post('/fetch', function (req, res) {
+  headlinesController.check(function(data) {
+    res.json(data);
+  });
 });
 
