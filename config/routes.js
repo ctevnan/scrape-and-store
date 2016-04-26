a2127890ad8256731fb23c0e3e203d1317e9943e
ctevnan <carolyntevnan@yahoo.com>
2016-04-26 13:31:36 -0400

    adding routes

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,7 @@ var scrape = require('../scripts/scrape.js');
 var headlinesController = require('../controllers/headlinesController.js');
 var notesController = require('../controllers/notes.js');
 
-//use callback to return json data 
+//use callback to return json data from mongo
 router.get('/', function (req, res) {
   res.render('home');
 });
@@ -18,8 +18,30 @@ router.get('/test', function (req, res) {
 
 //get web scrape
 router.post('/fetch', function (req, res) {
+  headlinesController.fetch(); {
+  res.send('success');
+});
+
+//check mongo for data
+router.get('/check', function (req, res) {
   headlinesController.check(function(data) {
     res.json(data);
   });
 });
+
+//gather notes from mongo
+router.post('/gather', function (req, res) {
+  notesController.gather(req.body, function(data) {
+    res.json(data);
+  });
+});
+
+//post to save note into mongo
+router.post('/save', function (req, res) {
+  notesController.save(req.body, function(data) {
+    res.json(data);
+  });
+});
+
+
 
