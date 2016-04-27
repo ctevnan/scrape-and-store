f0810dc6fb08172fd34ce3825f092d86cc4b63ed
ctevnan <carolyntevnan@yahoo.com>
2016-04-27 02:50:30 -0400

    adding styling

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -3,3 +3,62 @@ var makeDate = require('../scripts/date.js');
 var Headline = require('../models/headlinemodel.js');
 var Note = require('../models/notemodel.js');
 
+//export as fetch
+exports.fetch = function() {
+
+  //scrape funct
+  scrape("http://www.cnn.com", function(data) {
+    var obj = data;
+
+    //create date
+    var formattedDate = makedate();
+
+    //loop over obj results
+    for (var i in obj) {
+      addIfNotFound(i);
+    } 
+
+    //check to see if entry exists add if not
+    function addIfNotFound(current) {
+      //find one by headline
+      Headline.findOne({
+        'headline': obj[current][0]
+      }, function(err, res) {
+        if (err) {
+          console.log(err);
+        }
+        //if no match
+        if (res === null) {
+          //new entry obj
+          var headlineEntry = new Headline({
+            headline: obj[current][0],
+            summary: obj[current][1],
+            date: formattedDate
+          });
+          //save new entry to db
+          headlineEntry.save(function(err) {
+            if (err) {
+              // console.log(err);
+            } else {
+              console.log('successfully added');
+            }
+          });
+        }
+      });
+    }
+
+  });
+};
+
+//export this function as "check"
+exports.check = function(cb) {
+  //get scraped data, sort starting from most recent
+  Headline.find()
+    .sort({
+      _id: -1
+    })
+    .exec(function(err, doc) {
+      //once finished, pass the list into the callback function
+      cb(doc);
+    });
+};
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,3 +1,48 @@
 var makeDate = require('../scripts/date.js');
 var Headline = require('../models/headlinemodel.js');
 var Note = require('../models/notemodel.js');
+
+exports.save = function(data, cb) {
+  //create date
+  var formattedDate = makeDate();
+
+        var newNote = new Note ({
+          _headlineId:data.id,
+          date:data.date,
+          noteText:data.note
+        });
+
+        newNote.save(function(err, doc){
+          if (err) {
+            console.log(err);
+        } else{
+          console.log(doc);
+          cb(doc);
+        }
+      });
+    };
+
+  exports.gather = function(data, cb) {
+      Note.find({
+          _headlineId: data.id
+      })
+      .sort({
+        id: -1
+      })
+      .exec(function(err, doc) {
+          cb(doc);
+      });
+  };
+
+  exports.delete = function(data, cb) {
+      Note.remove({
+        _headlineId:data.id
+      }, function(err, removed){
+        if(err){
+          console.log(err);
+        } else {
+          console.log("Delete Sucessful");
+          cb(removed);
+        }
+      });
+  };  
