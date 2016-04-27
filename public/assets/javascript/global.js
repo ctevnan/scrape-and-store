1125e2d864269c8c0c89c031a35d87a3a3a8ee39
ctevnan <carolyntevnan@yahoo.com>
2016-04-27 02:08:45 -0400

    save notes and delete notes funct

--- public/assets/javascript/global.js
+++ public/assets/javascript/global.js
@@ -21,5 +21,104 @@ var mongoData;
 var dataCount = 0;
 var dataDate;
 
-    
+//rorating cube state
+var state = 0;
+var cubeRotateAry = ['show-front', 'show-back', 'show-right', 'show-left', 'show-top', 'show-bottom'];
+var sideAry = ['back', 'right', 'left', 'top', 'bottom', 'front'];
+
+//ajax get news
+var populate = function() {
+  //get JSON with jquery AJAX call
+  $.getJSON('/check', function(data) {
+    mongoData = data;
+    dataDate = mongoData[mongoData.length -1].date;
+    //for item in json add table row and cells to content string
+  }).done(function() {
+    //running clickbox funct
+    clickBox();
+    saveNote();
+  });
+};
+
+//ajax get note data
+var gather = function() {
+  var idCount = dataCount - 1;
+
+  //get JSON
+  $.ajax({
+    type: "POST",
+    dataType: "json",
+    url: '/gather',
+    data: {
+      id: mongoData[idCount]._id
+    }
+  })
+  .done(function(currentNotes) {
+    postNote(currentNotes);
+  })
+  .fail(function() {
+    console.log("Apologies. Server unavailable.");
+  });
+};
+
+//render notes from data
+var postNote = function(currentNotes) {
+  $("note-box").val("");
+
+  var note = "";
+  for (var i = 0; i < currentNotes.length; i++) {
+    note = note + currentNotes[i].noteText + '\n';
   }
+
+  $("#note-box").val(note);
+};
+
+//function with listener to save notes and clear note taking area
+var saveNote = function() {
+  $("#note-button").on('click', function() {
+    var text = $("#input-box").val();
+    var idCount = dataCount - 1;
+
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: '/save',
+      data: {
+        id: mongoData[idCount]._id,
+        date: dataDate,
+        note: text
+      }
+    })
+    .done(function() {
+      $("#input-box").val("");
+      //get notes again bc new note saved
+      gather();
+    })
+    .fail(function() {
+      console.log("Apologies. Server down");
+    });
+  });
+};
+
+//function with listener to delete notes      
+var deleteNote = function() {
+  $("#delete-button").on('click', function() {
+    var idCount = dataCount - 1;
+
+    $.ajax({
+      type: "DELETE",
+      dataType: "json",
+      url: '/delete',
+      data: {
+        id: mongoData[idCount]._id,
+      }
+    })
+    .done(function() {
+      $("#note-box").val("");
+    })
+    .fail(function() {
+      console.log("Apologies. Server down");
+    });
+
+  });
+};
