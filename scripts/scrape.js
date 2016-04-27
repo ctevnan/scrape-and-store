4dc5d82620de9d1dea86a4cef309e9d54458f199
ctevnan <carolyntevnan@yahoo.com>
2016-04-27 01:26:46 -0400

    using request and cheerio to scrape cnn.articles will be grouped by the class theme-summary and jquery's .children() used to find headings, summaries, then the data will be filtered and trimmed down

---
+++ scripts/scrape.js
@@ -0,0 +1,38 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+//scrape cnn
+var scrape = function(url, cb) {
+  if (url == "http://www.cnn.com") {
+    request(url, function(err, res, body) {
+      var $ = cheerio.load(body);
+      var obj = {};
+
+      $('.theme-summary').each(function(i, element){
+        //articles grouped by class theme-summary
+        //use jQuery .children() to find headings, summaries
+        var head =$(this).children(".story-heading").text();
+        var sum = $(this).children(".summary").text();
+
+        if (head !== "" && sum !== ""){
+          //filter data, trim
+          var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+          var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+          obj[i] = [headNeat]; //init array with headneat 
+          obj[i].push(sumNeat);
+        }
+      });
+      console.log(obj); //for testing
+
+      cb(obj);
+    });
+  }
+};
+
+module.exports = scrape;          
+        }
+      })
+    })
+  }
+}
