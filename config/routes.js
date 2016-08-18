119238b4b7314aa693b636cbac2f6f3c21423475
ctevnan <carolyntevnan@yahoo.com>
2016-08-18 11:51:04 -0400

    delinting routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,51 +1,51 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var scrape = require('../scripts/scrape.js');
-var headlinesController = require('../controllers/headlines.js');
-var notesController = require('../controllers/notes.js');
+var scrape = require("../scripts/scrape.js");
+var headlinesController = require("../controllers/headlines.js");
+var notesController = require("../controllers/notes.js");
 
 //use callback to return json data from mongo
-router.get('/', function (req, res) {
-  res.render('home');
+router.get("/", function (req, res) {
+  res.render("home");
 });
 
 //route to test scrape
-router.get('/test', function (req, res) {
+router.get("/test", function (req, res) {
   scrape("http://www.cnn.com", function (data) {
     res.json(data);
   });
 });
 
 //get web scrape
-router.post('/fetch', function (req, res) {
+router.post("/fetch", function (req, res) {
   headlinesController.fetch(); {
-  res.send('success');
+  res.send("success");
 };
 
 //check mongo for data
-router.get('/check', function (req, res) {
+router.get("/check", function (req, res) {
   headlinesController.check(function(data) {
     res.json(data);
   });
 });
 
 //gather notes from mongo
-router.post('/gather', function (req, res) {
-  notesController.gather(req.body, function(data) {
+router.post("/gather", function (req, res) {
+  notesController.gather(req.body, function (data) {
     res.json(data);
   });
 });
 
 //post to save note into mongo
-router.post('/save', function (req, res) {
-  notesController.save(req.body, function(data) {
+router.post("/save", function (req, res) {
+  notesController.save(req.body, function (data) {
     res.json(data);
   });
 });
 
 //delete note from mongo
-router.delete('/delete', function (req, res) {
-  notesController.delete(req.body, function(data) {
+router.delete("/delete", function (req, res) {
+  notesController.delete(req.body, function (data) {
     res.json(data);
   });  
 });
